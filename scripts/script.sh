53979502fd64a37731528f82128edbf06b093fe2
Pascal Cohen <pcohen@veepee.com>
2024-03-21 15:39:51 +0100

    update workflow

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/bash
 
 echo My Script
-LINES=`wc -l README.md`
+LINES=`wc -l < README.md`
 echo README has $LINES lines
 
-LINES2=3
-
-if [ $LINES2 -lt 5 ]
+if [ $LINES -lt 5 ]
 
 then
 	echo not enough lines
